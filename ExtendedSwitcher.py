98b554ee84b095635b3b0ab21703ab01bde66a9c
evandrocoan <evandrocoan@hotmail.com>
2017-11-04 22:31:45 -0200

    Set to automatically show to the selected view when attempting to
    switch. Issue:
    Set to ctrl+shift+tab to automatically show the tab when selected
    https://github.com/evandrocoan/SublimeStudio/issues/64

--- ExtendedSwitcher.py
+++ ExtendedSwitcher.py
@@ -14,16 +14,19 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 		self.open_views = []
 		self.window = sublime.active_window()
 		self.settings = sublime.load_settings('ExtendedSwitcher.sublime-settings')
+		self.active_view = self.window.active_view()
+
 		folders = self.window.folders()
-		
-		for f in self.getViews(list_mode):
+		active_view_id = self.active_view.id()
+
+		for view in self.getViews(list_mode):
 			# if skip the current active is enabled do not add the current file it for selection
 			if self.settings.get('skip_current_file') == True:
-				if f.id() == self.window.active_view().id():
+				if view.id() == active_view_id:
 					continue
 
-			self.open_views.append(f) # add the view object
-			file_name = f.file_name() # get the full path
+			self.open_views.append(view) # add the view object
+			file_name = view.file_name() # get the full path
 			file_path = ''
 
 			if file_name:
@@ -31,35 +34,42 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 					if os.path.commonprefix([folder, file_name]) == folder:
 						file_path = os.path.relpath(file_name, folder)
 
-				if f.is_dirty():
+				if view.is_dirty():
 					file_name += self.settings.get('mark_dirty_file_char') # if there are any unsaved changes to the file
 
 				if self.settings.get('show_full_file_path') == True:
-					self.open_files.append([os.path.basename(file_name), file_path]) 
+					self.open_files.append([os.path.basename(file_name), file_path])
 				else:
-					self.open_files.append([os.path.basename(file_name), '']) 
-			elif f.name():
-				if f.is_dirty():
-					self.open_files.append([f.name() + self.settings.get('mark_dirty_file_char'), ''])
+					self.open_files.append([os.path.basename(file_name), ''])
+			elif view.name():
+				if view.is_dirty():
+					self.open_files.append([view.name() + self.settings.get('mark_dirty_file_char'), ''])
 				else:
-					self.open_files.append([f.name(), ''])
+					self.open_files.append([view.name(), ''])
 			else:
-				if f.is_dirty():
+				if view.is_dirty():
 					self.open_files.append(["Untitled"+self.settings.get('mark_dirty_file_char'), ''])
 				else:
 					self.open_files.append(["Untitled", ''])
 
 		if self.check_for_sorting() == True:
 			self.sort_files()
- 
-		self.window.show_quick_panel(self.open_files, self.tab_selected, False, -1) # show the file list
+
+		def on_selection(selected):
+
+			if selected > -1:
+				self.window.focus_view(self.open_views[selected])
+
+		self.window.show_quick_panel(self.open_files, self.tab_selected, 0, -1, on_selection) # show the file list
 
 	# display the selected open file
 	def tab_selected(self, selected):
+
 		if selected > -1:
 			self.window.focus_view(self.open_views[selected])
 
-		return selected
+		else:
+			self.window.focus_view(self.active_view)
 
 	# sort the files for display in alphabetical order
 	def sort_files(self):
@@ -68,31 +78,27 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 
 		open_files.sort()
 
-		for f in open_files:
-			f = f[0]
+		for file_path in open_files:
+			file_path = file_path[0]
 			for fv in self.open_views:
 				if fv.file_name():
-					f = f.replace(" - " + os.path.dirname(fv.file_name()),'')
-					if (f == os.path.basename(fv.file_name())) or (f == os.path.basename(fv.file_name())+self.settings.get('mark_dirty_file_char')):
+					file_path = file_path.replace(" - " + os.path.dirname(fv.file_name()),'')
+					if (file_path == os.path.basename(fv.file_name())) or (file_path == os.path.basename(fv.file_name())+self.settings.get('mark_dirty_file_char')):
 						open_views.append(fv)
 						self.open_views.remove(fv)
-				elif fv.name() == f or fv.name()+self.settings.get('mark_dirty_file_char') == f:
+				elif fv.name() == file_path or fv.name()+self.settings.get('mark_dirty_file_char') == file_path:
 					open_views.append(fv)
 					self.open_views.remove(fv)
-				elif f == "Untitled" and not fv.name():
+				elif file_path == "Untitled" and not fv.name():
 					open_views.append(fv)
 					self.open_views.remove(fv)
 
-				
 		self.open_views = open_views
-
-
 
 	# flags for sorting
 	def check_for_sorting(self):
 		if self.settings.has("sort"):
 			return self.settings.get("sort", False)
-
 
 	def getViews(self, list_mode):
 		views = []
